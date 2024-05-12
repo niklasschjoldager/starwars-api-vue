e3499823778030b88e4fd7c0e3c27c182148d4ef
Niklas Vejlby Schjoldager <61137786+niklasschjoldager@users.noreply.github.com>
2024-05-12 15:03:12 +0200

    Basic fetch & displaying of data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
 
-<template><h1>This is a home page</h1></template>
+const data = ref(null)
+const error = ref(null)
+
+fetch('https://swapi.dev/api/people')
+  .then((res) => res.json())
+  .then((json) => (data.value = json))
+  .catch((err) => (error.value = err))
+</script>
+
+<template>
+  <h1>This is a home page</h1>
+  <div v-if="error">Oops, an error occured: {{ error.message }}</div>
+  <div v-else-if="!data">Loading...</div>
+  <div v-else>
+    <div v-for="({ name, height }, index) in data.results" :key="index">
+      <article class="">
+        <h2>{{ name }}</h2>
+        <p>{{ height }}</p>
+      </article>
+    </div>
+  </div>
+</template>
