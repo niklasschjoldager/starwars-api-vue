eb154363bb5d4e36fc013f3122809a362fc40b2c
Niklas Vejlby Schjoldager <61137786+niklasschjoldager@users.noreply.github.com>
2024-05-12 14:33:37 +0200

    Added TailwindCSS, removed unused stuff

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<template><h1>This is a home page</h1></template>
